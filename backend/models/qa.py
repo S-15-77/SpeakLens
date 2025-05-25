b46b4a172cd3680d404dc6bf67a94350c74553ac
S-15-77 <santhoshdayakar2002@gmail.com>
2025-05-25 12:23:58 -0400

    minor comment changes

--- backend/models/qa.py
+++ backend/models/qa.py
@@ -5,8 +5,4 @@ class QuestionAnswerModel:
         self.qa_pipe = pipeline("question-answering",model="deepset/roberta-base-squad2")
     def answer(self,content:str,question:str) -> str:
         result = self.qa_pipe(question=question,context=content)
-        # print("+++++++++++++++++++++")
-        # print(result)
-        # print("+++++++++++++++++++++")
         return result['answer']
-        # return result
